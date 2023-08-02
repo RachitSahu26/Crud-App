2bdde87cc59a67047f135e3808174acabd0014bc
rachit <curiousrachit.26@gmail.com>
2023-08-02 22:52:55 +0530

    doc-added a top section search and app name

---
+++ Crud App/src/Componerts/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="h-[60px] justify-center items-center bg-white m-4 rounded flex" >
+      <img src="public/Vector.png"></img>
+      <h1>Fire Base Contact App</h1>
+
+    </div>
+  )
+}
